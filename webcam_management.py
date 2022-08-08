a6c0893a2e7336622311190e1854fcaf6e7f9c55
patlukas <lukaszewski.patryk06@gmail.com>
2022-08-08 08:51:42 +0200

    Zmiany w webcam_management.py: 1. Usunięcie nieużywanej bibliotek Union 2. Poprawienie kilku literówek w komentarzach

--- webcam_management.py
+++ webcam_management.py
@@ -1,18 +1,18 @@
 """Moduł odpowiedzialny za zarządzanie kamerą (pobieranie listy kamer, wybieranie kamery i dawanie klatki w kamery."""
+
 import numpy as np
 import cv2
-from typing import Union
 
 
 class WebcamManagement:
     """
     Obiekt do zarządzania kamerami, wybierania kamer i dostarczania obrazu z kamer.
 
-    get_list_index_webcam() -> list[int] - pobranie indeksów dostępnych kamer
-    change_index_chosen_webcam(int) -> bool - zmiana wybranej kamery
-    get_frame_from_webcam(int) -> False|np.ndarray - jeżeli int podany to zmiana kamery. Podanie klatki obrazu
+    get_list_index_webcam() -> list[str] - pobranie indeksów dostępnych kamer
+    change_index_chosen_webcam(str) -> bool - zmiana wybranej kamery
+    get_frame_from_webcam(str) -> False|np.ndarray - jeżeli int podany to zmiana kamery. Podanie klatki obrazu
     remove_chosen_webcam() -> None - usuwa z pamięci obiektu która kamera była używana
-    get_index_chosen_webcam() -> int - zwraca index kamery aktualnie używanej
+    get_index_chosen_webcam() -> str - zwraca index kamery aktualnie używanej
     """
     def __init__(self):
         """
@@ -21,16 +21,15 @@ class WebcamManagement:
                               przez użytkownika musi być na tej liście
         __chosen_webcam_videocapture - jeżeli użytkowik wybrał kamerę to tu będzie obiekt cv2.VideoCapture tej kamery
         """
-        self.__chosen_index_webcam: int = -1
-        self.__list_index_webcam: list[int] = []
-        self.__chosen_webcam_videocapture: Union[None, cv2.VideoCapture] = None
+        self.__chosen_index_webcam: str = "-1"
+        self.__list_index_webcam: list[str] = []
+        self.__chosen_webcam_videocapture: None | cv2.VideoCapture = None
 
-    def get_list_index_webcam(self) -> list[int]:
+    def get_list_index_webcam(self) -> list[str]:
         """
         Metoda do wykrycia pod jakimi indeksami są dostępne kamery.
 
-        Indeksy są sprawdzane od 0 do momentu, aż jest  dostępna kamera o podanym indeksie, pozatym są odrzucane kameru,
-        których obraz jest praktycznie w pełni czarny.
+        Indeksy są sprawdzane od 0 do 10, pozatym są odrzucane kamery, których obraz jest praktycznie w pełni czarny.
         :return: lista indeksów z dostępnymi kamerami
         """
         index = 0
@@ -44,42 +43,48 @@ class WebcamManagement:
                 frame = cv2.cvtColor(src=frame, code=cv2.COLOR_BGR2GRAY)
                 frame = cv2.threshold(src=frame, thresh=10, maxval=1, type=cv2.THRESH_BINARY)[1]
                 if np.sum(frame) > 100 * 200 * 0.1:
-                    list_webcam.append(index)
-                cap.release()
+                    list_webcam.append(str(index))
+            cap.release()
             index += 1
         self.__list_index_webcam = list_webcam
+        if len(list_webcam):
+            self.change_index_chosen_webcam(list_webcam[0])
         return list_webcam
 
-    def change_index_chosen_webcam(self, new_index: int) -> bool:
+    def change_index_chosen_webcam(self, new_index: str) -> bool:
         """
         Metoda do zmiany wybranej kamery.
 
         Metoda sprawdza czy pod wybranym indeksem jest dostępna kamera (została wykryta) oraz próbuje nawiązać
-        połączenie. Jak się coś nie uda to zwraca Fasle, jk wszystko się udało zwraca True.
-        :param new_index: numer kamery
+        połączenie. Jak się coś nie uda to zwraca Fasle, jak wszystko się udało zwraca True.
+
+        :param new_index: <str> numer kamery
+        return: <bool> True jeżeli kamera została mieniowa, False jeżeli
         """
         if new_index not in self.__list_index_webcam:
             return False
+        print("Wybrana kamera "+new_index)
         self.remove_chosen_webcam()
         self.__chosen_index_webcam = new_index
-        obj_videocapture = cv2.VideoCapture(new_index, cv2.CAP_DSHOW)
+        obj_videocapture = cv2.VideoCapture(int(new_index), cv2.CAP_DSHOW)
         if obj_videocapture.isOpened():
             self.__chosen_webcam_videocapture = obj_videocapture
+            self.__chosen_webcam_videocapture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+            self.__chosen_webcam_videocapture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
             return True
         else:
             return False
 
-    def get_frame_from_webcam(self, new_index: int = -1) -> Union[bool, np.ndarray]:
+    def get_frame_from_webcam(self, new_index: str = "-1") -> bool | np.ndarray:
         """
         Metoda do pobrania aktualnego obrazu z kamery. Dodatkowo można przed pobraniem zmienić wybraną kamerę.
 
         :param new_index: jeżeli różne od -1 to znaczy że użytkownik chce zmienić kamerę
         :return: False jeżeli być błąd, np.ndarray (obraz z kamery) jeżeli wszysko się udało
         """
-        if new_index != -1:
+        if new_index != "-1":
             if not self.change_index_chosen_webcam(new_index):
                 return False
-
         if self.__chosen_webcam_videocapture is None:
             return False
         if not self.__chosen_webcam_videocapture.isOpened():
@@ -95,8 +100,12 @@ class WebcamManagement:
         if self.__chosen_webcam_videocapture is not None:
             self.__chosen_webcam_videocapture.release()
         self.__chosen_webcam_videocapture = None
-        self.__chosen_index_webcam = -1
+        self.__chosen_index_webcam = "-1"
 
-    def get_index_chosen_webcam(self) -> int:
-        """Zwraca aktualny index używanej kamery, jak nie jest wybrana to -1."""
+    def get_index_chosen_webcam(self) -> str:
+        """
+        Zwraca aktualny index używanej kamery, jak nie jest wybrana to -1.
+
+        return: <int> index wybranej kamery, jak nie zoastałą wybrana to zwrócone zostanie -1
+        """
         return self.__chosen_index_webcam
