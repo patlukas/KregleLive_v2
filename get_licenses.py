388895c9eeaaeefe829436bf654c3173069ba6ec
patlukas <lukaszewski.patryk06@gmail.com>
2022-08-11 13:43:19 +0200

    Zmiany w get_licenses.py: 1. Poprawienie komentarzy.

--- get_licenses.py
+++ get_licenses.py
@@ -1,20 +1,26 @@
 """Moduł odpowiedzialny za zarządzanie licencjami graczy."""
+
 import json
 import gspread
-import logging
-import logging_config
 import google.auth
+from informing import Informing
 
 
 class GetLicenses:
     """
     Klasa odpowiedzialna za zarządzanie licencjami.
 
+    set_list_category_and_name_of_game(list[str], str) -> None - ustawia podstawowe ustawienia
     get_list_team(list[str], str) -> list[str] - metoda zwraca listę drużyn, w których są gracze spełniający warunki
     get_list_player_in_team(list[str], str, str) -> list[str] - metoda zwraca listę graczy spełniających warunki
     """
     def __init__(self, path_to_json: str, path_config_spreadsheets: str):
         """
+        :param path_to_json: ścieżka do pliku json z ustawieniami
+        :param path_config_spreadsheets: ścieżka do pliku json z konfiguracją klienta
+
+        self.__list_category - lista kategorii. Do jednej z nich musi należeć zawodnik
+        self.__name_of_game - nazwa rodzaju gry, np superliga mężczyzn, potrzebne gdy zawodnik jest wypożyczony
         self.__settings - słownik z ustawieniami plików z licencjami (pobierany z json)
         self.__licenses_data - lista słowników z danymi graczy, każdy element listy jest następująy:
                                 {
@@ -25,6 +31,8 @@ class GetLicenses:
                                 }
                             Jeżeli __licenses_data jest None to znaczy że nie udało się pobrać danych graczy.
         """
+        self.__list_category: list[str] = []
+        self.__name_of_game: str = ""
         self.__settings: dict = self.__get_settings_from_json(path_to_json)
         self.__licenses_data: list[dict] | None = self.__get_data_from_google_sheet(path_config_spreadsheets)
 
@@ -40,7 +48,7 @@ class GetLicenses:
             file = open(path_to_json, encoding='utf8')
             return json.load(file)
         except FileNotFoundError:
-            logging.warning(f"Nie można odczytać ustawień licencji z pliku {path_to_json}")
+            Informing().warning(f"Nie można odczytać ustawień licencji z pliku {path_to_json}")
             return None
 
     def __get_data_from_google_sheet(self, path_config_spreadsheets: str) -> list[dict] | None:
@@ -85,55 +93,58 @@ class GetLicenses:
         Metoda próbuje pobrać ustawienia klienta połączenia i stworzyć jego obiekt, inaczej None
 
         :param path_config_spreadsheets: ścieżka do json z konfiguracją klienta
-        :return: obiekt klienta - jak udało się połączć, None jak połączenie się nie udało
+        :return: obiekt klienta - jak udało się połączć, None - jak połączenie się nie udało
         """
         try:
             return gspread.service_account(filename=path_config_spreadsheets)
         except FileNotFoundError:
-            logging.warning(f"Nie znaleziono pliku z konfiguracją klienta {path_config_spreadsheets}")
+            Informing().warning(f"Nie znaleziono pliku z konfiguracją klienta {path_config_spreadsheets}")
             return None
 
-    def get_list_team(self, list_category: list, type_of_game: str = "") -> list[str]:
+    def set_list_category_and_name_of_game(self, list_category: list[str], name_of_game: str) -> None:
+        """
+        Metoda ustawia jakie kategorie mają być brane pod uwagę podczas przeszukiwania licencji.
+
+        :param list_category: lista dozwolonych kategorii,  np ["Młodzik", "Senior", "Junior"]
+        :param name_of_game: nazwa rodzaju gry, np superliga mężczyzn (potrzebne w razie wypożyczeń)
+        """
+        self.__list_category = list_category
+        self.__name_of_game = name_of_game.lower()
+
+    def get_list_team(self) -> list[str]:
         """
         Metoda zwraca drużyn zawierających graczy (przynajmniej 1 gracza), którzy spełniają podane wymagania.
 
-        :param list_category: lista kategorii, np ["Młodzik", "Senior", "Junior"]
-        :param type_of_game: nazwa typu gry, np "superliga mężczyzn", jest to istotne w przypadku wypożyczeń.
-                             Jeżeli nie zostanie podany napis to będą brane tylko pod uwagę kluby maicerzyste.
         :return: Lista drużyn. Pod 0 indexem jest pusty ciąg znaków
         """
         if self.__licenses_data is None:
             return [""]
         teams = {"": 1}
         for player in self.__licenses_data:
-            if player["category"] not in list_category:
+            if player["category"] not in self.__list_category:
                 continue
-            team = player.get(type_of_game.lower(), player["team"])
+            team = player.get(self.__name_of_game, player["team"])
             teams[team] = 1
         teams_list = [*teams]
         teams_list.sort()
         return teams_list
 
-    def get_list_player_in_team(self, list_category: list[str], name_team: str, type_of_game: str = "") -> list[str]:
+    def get_list_player_in_team(self, name_team: str) -> list[str]:
         """
         Metoda zwraca listę graczy należących spełniających podane wymagania.
 
-        :param list_category: lista kategorii, gracz musi należeć do którejś z nich
         :param name_team: nazwa drużyny lub "". Jak "" to waunek pomiajny, jak nie to musi to być jego macierzysty klub
                           lub to musi być jego klub do którego został wypożyczony
-        :param type_of_game: jeżeli gracz jest wypożyczony na te rozgrywki to musi, być wypożyczony do drużyny name_team
-                             wtedy nie uwzględnia się macierzystego klubu
         :return: lista graczy. Pod 0 indexem jest pusty napis.
         """
         if self.__licenses_data is None:
             return [""]
         players = [""]
         for player in self.__licenses_data:
-            if player["category"] not in list_category:
+            if player["category"] not in self.__list_category:
                 continue
-            if name_team != "" and player.get(type_of_game.lower(), player["team"]) != name_team:
+            if name_team != "" and player.get(self.__name_of_game, player["team"]) != name_team:
                 continue
             players.append(player["name"])
         players.sort()
         return players
-
